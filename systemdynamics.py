76a433f7016dfa46fcd27ce2353e8e115c769cf2
Andreas Paul Zischg <andreas.zischg@giub.unibe.ch>
2022-12-15 12:55:18 +0100

    Update systemdynamics.py

--- systemdynamics.py
+++ systemdynamics.py
@@ -1,5 +1,5 @@
 #system dynamics model
-#example Lake level - Lake outflow system Lake Thun
+#example Lake level - Lake outflow system
 
 import xlrd
 import openpyxl
@@ -13,19 +13,21 @@ codespace="C:/DATA/develops/lakesystemdynamicsexample"
 timesteps=range(0,73,1) #3 days
 deltat=1 #hours
 max(timesteps)
-lakearea= 48300000
-initial_lakelevel=558
 addedvolume=0
+lakearea= 48300000
+#parameters to play with
+initial_lakelevel=558
 lakelevel=initial_lakelevel
 initialoutflow=200.0
 tunnelcapacity=100.0
 securitylevel=400.0
 timelag=24
 
+#read the input data (non-linear relationships)
 inflowdf=pd.read_excel(codespace+"/"+"inputdata.xlsx", dtype="float", engine='openpyxl', sheet_name="inflow")
 lakeleveldf=pd.read_excel(codespace+"/"+"inputdata.xlsx", dtype="float", engine='openpyxl', sheet_name="lakelevel")
 
-
+#plot the input data
 plt.plot(inflowdf.hour, inflowdf.inflow)
 plt.plot(lakeleveldf.lakelevel, lakeleveldf.increasevolume)
 plt.plot(lakeleveldf.lakelevel, lakeleveldf.outflow)
@@ -40,7 +42,7 @@ y_outflow=lakeleveldf.outflow.array
 f_outflow=interp1d(x_outflow, y_outflow, kind="linear")
 #f_outflow(558.2)
 
-#ohne Massnahmen
+#natural system without human intervention
 lakelevellist=[initial_lakelevel]
 outflowlist=[initialoutflow]
 addedvolume=0
@@ -63,7 +65,7 @@ for t in timesteps:
         print("inflow: "+str(inflow))
         print("outflow: " + str(outflow))
 
-#Mit Massnahme: Tunnel 100 m3/s wenn outflow < 400
+#with human intervention - the relief tunnel: Tunnel adds surplus outflow of 100 m3/s if outflow < 400 m3/s
 lakelevellist2=[initial_lakelevel]
 outflowlist2=[initialoutflow]
 addedvolume=0
@@ -89,7 +91,8 @@ for t in timesteps:
         lakelevellist2.append(f_lakelevel(addedvolume))
 
 
-#Mit Erosion: Wenn outflow > 300 dann outflow* 1.5
+#System behavior if outflow exceeds a tipping point of 300 m3/s. 
+#Above this outflow, discharge is increased by a factor of 1.5 due to lateral erosion
 lakelevellist3=[initial_lakelevel]
 outflowlist3=[initialoutflow]
 addedvolume=0
@@ -116,8 +119,7 @@ for t in timesteps:
 
 
 
-#Plot inflow-outflow
-
+#Plot inflow-outflow and lake level
 fig, axs = plt.subplots(1, 2, figsize=(9, 3), sharey=False)
 fig.suptitle('lake level feedback')
 axs[0].plot(inflowdf.hour, inflowdf.inflow, label="inflow")
@@ -135,9 +137,3 @@ axs[1].set_xlabel("hour")
 axs[1].set_ylabel("lake level [m a.s.l.]")
 plt.show()
 
-
-
-
-plt.plot(inflowdf.hour, inflowdf.inflow)
-plt.plot(inflowdf.hour.values.tolist(), outflowlist)
-plt.plot(inflowdf.hour.values.tolist(), outflowlist2)
